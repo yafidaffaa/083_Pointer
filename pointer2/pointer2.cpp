596b46de9f705f91d6137e7b92ce9e174b548e76
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-10 11:07:23 +0700

    membuat pointer new mhs, memberi nilai nim di mhs = 2 dan menampilkannya kemudian menghapus mhs agar tidak terus menerus dibuat pada saat program dijalankan, setelah dijalankan program di berhentikan sebentar / pause

--- pointer2/pointer2.cpp
+++ pointer2/pointer2.cpp
@@ -8,3 +8,12 @@ public:
 		cout << "No Induk = " << nim << endl;
 	}
 };
+
+int main() {
+	mahasiswa* mhs = new mahasiswa{ 1 };
+	mhs->nim = 2;
+	mhs->showNim();
+	delete mhs;
+	system("pause");
+	return 0;
+}
